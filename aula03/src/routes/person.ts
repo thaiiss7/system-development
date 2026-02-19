def0448395363162acb8ad26f99fe19a1edc8c3f
thaiiss7 <thaismichel7@gmail.com>
2026-02-19 11:39:46 -0300

    desafio

--- aula03/src/routes/person.ts
+++ aula03/src/routes/person.ts
@@ -4,32 +4,73 @@ const router: Router = express.Router();
 const people: object[] = [];
 
 router
-    .post('/registrar', (req: Request, res: Response) => {
-        const {name, lastname} = req.body
-        console.log(name, lastname)
-        people.push({name, lastname})
-        res.status(200).send({message: `Usuario cadastrado`})
+    // .post('/registrar', (req: Request, res: Response) => {
+    //     const {name, lastname} = req.body
+    //     console.log(name, lastname)
+    //     people.push({name, lastname})
+    //     res.status(200).send({message: `Usuario cadastrado`})
+    // })
+
+    // .get('/users', (req: Request, res: Response) => {
+    //     res.status(200).send({users: people})
+    // })
+
+    // .get('/users/:id', (req: Request, res: Response) => {
+    //     const { id } = req.params
+    //     let convertedId = Number(id)
+    //     res.status(200).send({ response: people[convertedId] })
+    // })
+
+    // .get('/filtro', (req: Request, res: Response) => {
+    //     const {name, lastname} = req.query
+    //     res.status(200).send({ response: name, lastname })
+    // })
+
+    // .put('/atualizar/:id', (req: Request, res: Response) => {
+    //     const { id } = req.params
+    //     const { name, lastname } = req.body
+    //     res.status(200).send({ response: `atualizando o usuario ${id} -> ${name} ${lastname}` })
+    // })
+
+    // desafio
+
+    // registrar usuários
+    .post('/register', (req: Request, res: Response) => {
+        const { id, name, age } = req.body
+        console.log(id, name, age)
+        people.push({ id, name, age})
+        res.status(200).send({message: `usuario ${name} cadastrado`})
     })
 
+    // acessar todos os usuários
     .get('/users', (req: Request, res: Response) => {
         res.status(200).send({users: people})
     })
 
-    .get('/users/:id', (req: Request, res: Response) => {
+    // acessar usuário por id
+    .get('/user/:id', (req: Request, res: Response) => {
         const { id } = req.params
         let convertedId = Number(id)
         res.status(200).send({ response: people[convertedId] })
     })
 
-    .get('/filtro', (req: Request, res: Response) => {
-        const {name, lastname} = req.query
-        res.status(200).send({ response: name, lastname })
+    //acessar usuário com filtro
+    .get('/filter', (req: Request, res: Response) => {
+        const {name, age} = req.query
+        res.status(200).send({ response: name, age })
     })
 
-    .put('/atualizar/:id', (req: Request, res: Response) => {
+    // atulizar dados
+    .put('/update/:id', (req: Request, res: Response) => {
         const { id } = req.params
-        const { name, lastname } = req.body
-        res.status(200).send({ response: `atualizando o usuario ${id} -> ${name} ${lastname}` })
+        const { name, age } = req.body
+        res.status(200).send({ response: `atualizando o usuario ${id} -> ${name} ${age}` })
+    })
+
+    // deletar por id
+    .delete('/delete/:id', (req: Request, res: Response) => {
+        const { id } = req.params
+        res.status(200).send({ response: `usuario deletado`})
     })
 
 //....
